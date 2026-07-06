ebc1535e976aefe9840dc291bbfc8481f10ac2be
4evy <git@evy.pink>
2026-07-06 18:15:00 +0300

    helium: store app files under data root

--- ansible/files/scripts/apps/install-helium-linux.sh
+++ ansible/files/scripts/apps/install-helium-linux.sh
@@ -13,22 +13,26 @@ source_host_lib json
 # shellcheck source=ansible/files/scripts/apps/helium-lib.sh
 source -p "$script_dir" helium-lib.sh
 
-require_arg_count 4 5 "$@"
-root=$1
-bin_dir=$2
-installer_bin=$3
-secrets_file=$4
-flags=${5:-}
-require_value 'cache root' "$root"
+require_arg_count 5 6 "$@"
+cache_root=$1
+app_root=$2
+bin_dir=$3
+installer_bin=$4
+secrets_file=$5
+flags=${6:-}
+require_value 'cache root' "$cache_root"
+require_value 'app root' "$app_root"
 require_value 'bin dir' "$bin_dir"
 require_value 'installer bin dir' "$installer_bin"
-require_safe_path "$root"
+require_safe_path "$cache_root"
+require_safe_path "$app_root"
 require_safe_path "$bin_dir"
 require_safe_path "$installer_bin"
 require_file "$secrets_file"
 require_command go sops tar sed
 
-ensure_dir "$root"
+ensure_dir "$cache_root"
+ensure_dir "$app_root"
 ensure_dir "$bin_dir"
 ensure_dir "$installer_bin"
 
@@ -39,13 +43,14 @@ version=$(helium_release_tag "$release_json")
 asset_name="helium-${version}-${arch}_linux.tar.xz"
 download_url=$(helium_asset_download_url "$release_json" "$asset_name")
 digest=$(helium_asset_digest "$release_json" "$asset_name")
-archive="$root/$asset_name"
-extract_dir="$root/extract"
-app_dir="$root/app"
+archive="$cache_root/$asset_name"
+extract_dir="$cache_root/extract"
+app_dir="$app_root/app"
 version_file="$app_dir/.helium-version"
+old_cache_app_dir="$cache_root/app"
 
 if [[ ! -f $version_file || $(<"$version_file") != "$version" ]]; then
-	ensure_dir "$root"
+	ensure_dir "$cache_root"
 	helium_ensure_downloaded "$archive" "$download_url" "$digest"
 
 	printf '%s\n' 'helium-browser: extracting application archive' >&2
@@ -73,13 +78,16 @@ if [[ ! -f $version_file || $(<"$version_file") != "$version" ]]; then
 else
 	printf 'helium-browser: application %s is already installed\n' "$version" >&2
 fi
+if [[ $old_cache_app_dir != "$app_dir" && -e $old_cache_app_dir ]]; then
+	remove_path "$old_cache_app_dir"
+fi
 
 CGO_ENABLED=0 GOBIN="$installer_bin" go install ./cmd/helium-browser
 "$installer_bin/helium-browser" configure \
 	--secrets "$secrets_file" \
 	-- \
 	linux \
-	"$root" \
+	"$cache_root" \
 	"$app_dir" \
 	"$bin_dir" \
 	"$flags"
